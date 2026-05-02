90d493948e92df9eb693130024d58ccd0e30fbc3
Nisarg002 <nisargmistry36@gmail.com>
2026-05-02 17:22:48 +0530

    mail-fix

--- backend/utils/SendMail.py
+++ backend/utils/SendMail.py
@@ -9,10 +9,10 @@ import os
 
 load_dotenv()
 
-SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = os.getenv("SMTP_PORT")
+SMTP_SERVER = os.getenv("SMTP_SERVER") 
+SMTP_PORT = os.getenv("SMTP_PORT") 
 SMTP_EMAIL =os.getenv("SMTP_EMAIL")
-SMTP_PASSWORD=os.getenv("SMTP_PASSWORD")
+SMTP_PASSWORD=os.getenv("SMTP_PASSWORD") 
 
 def send_mail(to_email:str,subject:str,text:str):
     msg = MIMEMultipart()
